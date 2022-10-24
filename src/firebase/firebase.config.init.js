6ce99b4464811148208d7c3a6bcfe70dfca8d999
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-10-24 22:39:25 +0600

    updated readme

---
+++ src/firebase/firebase.config.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBSUZURLbV2rIR4a0b8FdotsR6YHGXGNBk",
+  authDomain: "code-crate.firebaseapp.com",
+  projectId: "code-crate",
+  storageBucket: "code-crate.appspot.com",
+  messagingSenderId: "192455981657",
+  appId: "1:192455981657:web:a79e43a22af1e9bdfc8f48",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
