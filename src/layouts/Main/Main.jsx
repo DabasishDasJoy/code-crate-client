3f90ae2028d93c1b5b0e84b519137de15c2fde9b
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-10-25 11:34:11 +0600

    header added

--- src/layouts/Main/Main.jsx
+++ src/layouts/Main/Main.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "../../pages/shared/Header/Header";
 
 const Main = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  );
 };
 
 export default Main;
